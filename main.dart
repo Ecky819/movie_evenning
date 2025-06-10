de3f00ad6a422d6b322ba9ad6cc81b0eea6271f0
Ecky819 <marcoeggert73@gmail.com>
2025-06-10 11:20:45 +0200

    Two if-statements

--- main.dart
+++ main.dart
@@ -1,11 +1,25 @@
 void main() {
-  int age = 25;
+  //test 1
+  int age = 15;
   bool hasParentalConsent = true;
-  int movieAgeRating = 18;
+  int movieAgeRating = 16;
 
-  if (age >= movieAgeRating || hasParentalConsent) {
-    print("Du kannst den Film sehen.");
+  //test 2
+  int age2 = 13;
+  bool hasParentalConsent2 = false;
+
+  //test 1
+  if (age >= movieAgeRating ||
+      (hasParentalConsent && age >= movieAgeRating - 2)) {
+    print("You can watch the movie.");
   } else {
-    print("Du kannst den Film nicht sehen.");
+    print("You cannot watch the movie.");
+  }
+  //test 2
+  if (age2 >= movieAgeRating ||
+      (hasParentalConsent2 && age2 >= movieAgeRating - 2)) {
+    print("You can watch the movie.");
+  } else {
+    print("You cannot watch the movie.");
   }
 }
