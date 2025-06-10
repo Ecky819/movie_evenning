c5a183c2ba6b0931ccc96d22b98061dcdebdc24c
Ecky819 <marcoeggert73@gmail.com>
2025-06-10 10:52:09 +0200

    variables initialize and if statement

--- main.dart
+++ main.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  int age = 25;
+  bool hasParentalConsent = true;
+  int movieAgeRating = 18;
+
+  if (age >= movieAgeRating || hasParentalConsent) {
+    print("Du kannst den Film sehen.");
+  } else {
+    print("Du kannst den Film nicht sehen.");
+  }
+}
